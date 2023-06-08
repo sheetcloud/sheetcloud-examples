4a8e89288696906662b0bd3212f81497b33ba5a5
nicococo <nicococo23@gmail.com>
2023-06-08 13:34:00 +0200

    Added a callback for sheetcloud.

--- app1.py
+++ app1.py
@@ -2,16 +2,23 @@ import pandas as pd
 import streamlit as st
 import sheetcloud as sc
 
-from datetime import datetime, timezone
-
+SHEET_URL = 'https://docs.google.com/spreadsheets/d/1VeLIJNw69pGmCY6GRkfabBUUYb5oh1rKA6jX5rPdpZs/edit#gid=1505618093'
+RSS_FEEDS = [('https://news.bitcoin.com/feed/', 'bitcoin_news_feed'), 
+             ('https://cointelegraph.com/rss/category/top-10-cryptocurrencies', 'coin_telegraph_top10')]
 
 def update():
-    df = pd.DataFrame(['=IMPORTFEED("https://news.bitcoin.com/feed/",,False)'], columns=['feed'])
-    sc.sheets.append('https://docs.google.com/spreadsheets/d/1VeLIJNw69pGmCY6GRkfabBUUYb5oh1rKA6jX5rPdpZs/edit#gid=1505618093', 'bitcoin_news_feed', df)
+    for url, sheet in RSS_FEEDS:
+        df = pd.DataFrame([f'=IMPORTFEED("{url}",,False)'], columns=['feed'])
+        sc.sheets.append(SHEET_URL, sheet, df)
 
 
-# update()
-df = sc.sheets.read('https://docs.google.com/spreadsheets/d/1VeLIJNw69pGmCY6GRkfabBUUYb5oh1rKA6jX5rPdpZs/edit#gid=1505618093', 'bitcoin_news_feed')
-# df.drop_duplicates(inplace=True)
-print(df)
-st.data_editor(df)
+if st.experimental_get_query_params().get('update', 'False').lower() == 'true':
+    update()
+else:
+    st.title('Crypto News Feed Collector')
+    st.markdown('Collect and store RSS feeds in Google Spreadsheets via Sheetcloud.')
+    df = sc.sheets.read(SHEET_URL, 'bitcoin_news_feed')
+    print(df.info())
+    df.drop_duplicates(inplace=True)
+    print(df.info())
+    st.data_editor(df, use_container_width=True)
