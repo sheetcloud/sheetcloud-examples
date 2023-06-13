200e4bd6e4164e381ecfa05e18466caf3037a41f
nicococo <nicococo23@gmail.com>
2023-06-13 14:18:38 +0200

    App1 now supports security token and settings

--- app1.py
+++ app1.py
@@ -1,24 +1,32 @@
 import pandas as pd
 import streamlit as st
 import sheetcloud as sc
+from datetime import datetime
 
+DEBUG = True
 SHEET_URL = 'https://docs.google.com/spreadsheets/d/1VeLIJNw69pGmCY6GRkfabBUUYb5oh1rKA6jX5rPdpZs/edit#gid=1505618093'
-RSS_FEEDS = [('https://news.bitcoin.com/feed/', 'bitcoin_news_feed'), 
-             ('https://cointelegraph.com/rss/category/top-10-cryptocurrencies', 'coin_telegraph_top10')]
-
-def update():
-    for url, sheet in RSS_FEEDS:
-        df = pd.DataFrame([f'=IMPORTFEED("{url}",,False)'], columns=['feed'])
-        sc.sheets.append(SHEET_URL, sheet, df)
 
 
-if st.experimental_get_query_params().get('update', ['False'])[0].lower() == 'true':
-    update()
-else:
-    st.title('Crypto News Feed Collector')
-    st.markdown('Collect and store RSS feeds in Google Spreadsheets via Sheetcloud.')
-    df = sc.sheets.read(SHEET_URL, 'bitcoin_news_feed')
-    print(df.info())
-    df.drop_duplicates(inplace=True)
-    print(df.info())
-    st.data_editor(df, use_container_width=True)
+def update(update_token: str):
+    config = sc.env.read(SHEET_URL, 'settings', False, False)
+    if config['update_token'] == update_token or DEBUG:
+        for k, v in config.items():
+            if k.lower().startswith('data_'):
+                items = config[f'values_{k[5:]}'].split(', ')
+                df = pd.DataFrame([[datetime.now(), v.replace('{}', item)] for item in items], columns=['timestamp', 'data'])
+                sc.sheets.append(SHEET_URL, k[5:], df, cache=False)
+
+
+update(st.experimental_get_query_params().get('update', [''])[0])
+
+# if st.experimental_get_query_params().get('update', ['False'])[0].lower() == 'true':
+#     update()
+# else:
+#     st.title('Crypto News Feed Collector')
+#     st.markdown('Collect and store RSS feeds in Google Spreadsheets via Sheetcloud.')
+#     df = sc.sheets.read(SHEET_URL, 'bitcoin_news_feed')
+#     print(df.info())
+#     df.drop_duplicates(inplace=True)
+#     print(df.info())
+#     st.data_editor(df, use_container_width=True)
+#     # sc.env.write(SHEET_URL, 'settings', {'token': '$1$xxabc$sp0653/HLmnBo0eb1V6q40', ''})
